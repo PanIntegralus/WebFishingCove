89b17574d25e6c075652b2366a20744d9c821078
PanIntegralus <pablikoxyt@gmail.com>
2024-11-28 01:25:08 +0100

    copy chunk

--- Cove.ChatCommands/ChatCommands.cs
+++ Cove.ChatCommands/ChatCommands.cs
@@ -294,12 +294,31 @@ public class ChatCommands : CovePlugin
                     foreach (ChalkCanvas canvas in Server.chalkCanvas)
                     {
                         canvas.loadCanvas();
-                        Dictionary<string, object> chalkPacket = new Dictionary<string, object>();
-                        foreach (KeyValuePair<int, object> entry in canvas.getChalkPacket())
+                        Dictionary<int, object> allChalk = canvas.getChalkPacket();
+
+                        // split the dictionary into chunks of 100
+                        List<Dictionary<int, object>> chunks = new List<Dictionary<int, object>>();
+                        Dictionary<int, object> chunk = new Dictionary<int, object>();
+
+                        int i = 0;
+                        foreach (var kvp in allChalk)
                         {
-                            chalkPacket[entry.Key.ToString()] = entry.Value;
+                            if (i >= 1000)
+                            {
+                                chunks.Add(chunk);
+                                chunk = new Dictionary<int, object>();
+                                i = 0;
+                            }
+                            chunk.Add(i, kvp.Value);
+                            i++;
                         }
-                        SendPacketToAll(chalkPacket);
+
+                        for (int index = 0; index < chunks.Count; index++)
+                        {
+                            Dictionary<string, object> chalkPacket = new Dictionary<string, object> { { "type", "chalk_packet" }, { "canvas_id", canvas.canvasID }, { "data", chunks[index] } };
+                            SendPacketToAll(chalkPacket);
+                            Thread.Sleep(10);
+                        }
                     }
                     break;
                 }
